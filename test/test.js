7a037c1c0a17617c379afb0bec93485892a9b335
Sean Lawlor <lawlorseanr@gmail.com>
2021-07-06 13:46:52 -0700

    Minor changes to counter and index

--- test/test.js
+++ test/test.js
@@ -134,7 +134,7 @@ describe('todos', () => {
   });
 
   describe('readOne', () => {
-    xit('should return an error for non-existant todo', (done) => {
+    it('should return an error for non-existant todo', (done) => {
       todos.readOne('notAnId', (err, todo) => {
         expect(err).to.exist;
         done();
